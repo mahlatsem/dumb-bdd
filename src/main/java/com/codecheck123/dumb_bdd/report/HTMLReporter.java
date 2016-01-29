86610bc6eda16d393b23b6cd3c92d842af6d73c0
mahlatsem <matt.mahlatse@gmail.com>
2016-01-29 13:22:42 +0200

    removed gibberish in preparation for writing functionality

--- src/main/java/com/codecheck123/dumb_bdd/report/HTMLReporter.java
+++ src/main/java/com/codecheck123/dumb_bdd/report/HTMLReporter.java
@@ -13,19 +13,6 @@ public class HTMLReporter extends Reporter {
 
 	@Override
 	public void write(){
-		System.out.println("UserStory: "+userStory);
-		List<String> givenStatements = given;
-		for(int i = 0; i < givenStatements.size(); i++){
-			if(i == 0){
-				System.out.println("Given: "+givenStatements.get(i));
-			}else {
-				System.out.println("And: "+givenStatements.get(i));
-			}
-		}
-		System.out.println("When: "+when);
-		System.out.println("Then: "+then);
-		if(assertionError != null){
-			System.out.println("Failed: "+assertionError.getMessage());
-		}
+		
 	}
 }
