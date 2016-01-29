3f195b2d4109711ca967ec1cf08e7d46c89d5730
mahlatsem <matt.mahlatse@gmail.com>
2016-01-29 13:24:18 +0200

    handle null or empty story

--- src/main/java/com/codecheck123/dumb_bdd/UserStory.java
+++ src/main/java/com/codecheck123/dumb_bdd/UserStory.java
@@ -3,8 +3,15 @@ package com.codecheck123.dumb_bdd;
 public class UserStory extends AbstractBDD {
 
 	private final String story;
-		
+	
+	/**
+	 * Throws EmptyExpressionException if story is NULL or an empty string.
+	 * @param story
+	 */
 	public UserStory(String story){
+		if(story == null || story.isEmpty()){
+			throw new EmptyExpressionException("Story may not be NULL or Empty");
+		}
 		this.story = story;
 	}
 	
