d209a21852ae3a5c3b0b4f54db333c1b527abe87
mahlatsem <matt.mahlatse@gmail.com>
2016-01-29 13:25:42 +0200

    turn into proper untility class and handle null input

--- src/main/java/com/codecheck123/dumb_bdd/ArgumentsParser.java
+++ src/main/java/com/codecheck123/dumb_bdd/ArgumentsParser.java
@@ -5,11 +5,16 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ArgumentsParser {
+public final class ArgumentsParser {
+	
+	private ArgumentsParser(){}
 	
 	private final static Pattern pattern = Pattern.compile("(?:^|\\s|,)'([^']*?)'");
 
-	public static List<String> parseExpressionToArguments(String givenInput) {
+	static List<String> parseExpressionToArguments(String givenInput) {
+		if(givenInput == null){
+			return new ArrayList<String>(0);
+		}
 		List<String> matches = new ArrayList<String>(5);
 		Matcher m = pattern.matcher(givenInput);
 		while(m.find()){
