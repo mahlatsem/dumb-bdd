c803a68c34ff25415809c85a31d5df3d2f45cb5b
mahlatsem <matt.mahlatse@gmail.com>
2016-01-29 13:27:15 +0200

    keep hidden to package

--- src/main/java/com/codecheck123/dumb_bdd/AbstractBDD.java
+++ src/main/java/com/codecheck123/dumb_bdd/AbstractBDD.java
@@ -3,7 +3,9 @@ package com.codecheck123.dumb_bdd;
 import java.util.List;
 
 public abstract class AbstractBDD {
-
+	
+	AbstractBDD(){}
+	
 	protected void evaluateExpression(String givenInput, ExpressionRunner runner) {
 		List<String> argsCollection = ArgumentsParser.parseExpressionToArguments(givenInput);
 		Arguments args = new Arguments(argsCollection);
