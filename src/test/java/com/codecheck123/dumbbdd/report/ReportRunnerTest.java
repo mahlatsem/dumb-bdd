c1d11b68925fb97bc0cd831d3293693028f85388
mahlatsem <matt.mahlatse@gmail.com>
2016-04-07 20:32:25 +0200

    cleaned up the test, eating own dog food

--- src/test/java/com/codecheck123/dumbbdd/report/ReportRunnerTest.java
+++ src/test/java/com/codecheck123/dumbbdd/report/ReportRunnerTest.java
@@ -4,29 +4,54 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
-import com.codecheck123.dumbbdd.Scenario;
+import com.codecheck123.dumbbdd.Arguments;
+import com.codecheck123.dumbbdd.ExpressionRunner;
 import com.codecheck123.dumbbdd.UserStory;
 
 public class ReportRunnerTest {
 
 	@Test
 	public void noExceptionsThrown() throws InterruptedException {
-		Scenario scenario = new UserStory.WithTitle("Reporting")
+		UserStory userStory = new UserStory.WithTitle("Reporting")
 				.asA("Developer")
 				.iCan("Have a report of my tests")
-				.create()
-				.scenarioWithTitle("Running reporter");
+				.create();
 		
-		Report report = new Report.ReportBuilder()
-				.setUserStoryTitle("Reporting")
-				.setUserStory("fill Story")
-				.setScenarioTitle("Reporting")
-				.setAllGiven(Arrays.asList("a","b"))
-				.setWhen("when")
-				.setThen("then")
-				.setAssertionError(null)
-				.build();
-		ReportRunner runner = new ReportRunner(report);
-		runner.run();
+		final Report.ReportBuilder reportBuilder = new Report.ReportBuilder();
+		
+		userStory.scenarioWithTitle("Success Scenario of Running a reporter")
+			.given("User story title 'Reporting'", new ExpressionRunner() {
+				public void expression(Arguments args) {
+					reportBuilder.setUserStoryTitle(args.first().asString());
+				}
+			})
+			.and("User story 'As a Developer, I can run reports'", new ExpressionRunner() {
+				public void expression(Arguments args) {
+					reportBuilder.setUserStory(args.first().asString());
+				}
+			})
+			.and("Scenario title 'Reporting' with given expressions 'a' and 'b'", new ExpressionRunner() {
+				public void expression(Arguments args) {
+					reportBuilder.setScenarioTitle(args.first().asString());
+					reportBuilder.setAllGiven(Arrays.asList(args.second().asString(),args.third().asString()));
+				}
+			})
+			.and("When 'I run' and then 'report should be published'", new ExpressionRunner() {
+				public void expression(Arguments args) {
+					reportBuilder.setWhen(args.first().asString());
+					reportBuilder.setThen(args.second().asString());
+				}
+			})
+			.when("I run the report", new ExpressionRunner() {
+				public void expression(Arguments args) {
+					Report report = reportBuilder.build();
+					ReportRunner runner = new ReportRunner(report);
+					runner.run(); //because if ran by a different thread, would run outside the test
+				}
+			})
+			.then("Should run with no errors", new ExpressionRunner() {
+				public void expression(Arguments args) {
+				}
+			});
 	}
 }
