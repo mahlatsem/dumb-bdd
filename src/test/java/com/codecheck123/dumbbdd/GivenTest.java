dd7ad561a5d88eebeb76cebb86fac8e47f9b4172
mahlatsem <matt.mahlatse@gmail.com>
2016-04-07 15:13:26 +0200

    renaming method names and using IllegalArgumentException

--- src/test/java/com/codecheck123/dumbbdd/GivenTest.java
+++ src/test/java/com/codecheck123/dumbbdd/GivenTest.java
@@ -12,18 +12,24 @@ import com.codecheck123.dumbbdd.Given;
 import com.codecheck123.dumbbdd.UserStory;
 
 public class GivenTest {
+	private UserStory userStory = new UserStory.WithTitle("Given Tests")
+			.asA("Developer")
+			.iCan("Write tests using bdd format")
+			.create();
 
 	@Test
-	public void testGetUserStory() {
-		Given given = new Given(new UserStory("story"), null, new ExpressionRunner() {
+	public void getUserStoryScenario() {
+		Scenario scenario = new Scenario(userStory, "Get a Scenario");
+		Given given = new Given(scenario, "Some given input expression", new ExpressionRunner() {
 			public void expression(Arguments args) {}
 		});
-		assertNotNull(given.getUserStory());
+		assertNotNull(given.getScenario());
 	}
 
 	@Test
-	public void testGetAll() {
-		Given given = new Given(new UserStory("story"), "1st given", new ExpressionRunner() {
+	public void getAllGivenInputs() {
+		Scenario scenario = new Scenario(userStory, "Get all Given inputs");
+		Given given = new Given(scenario, "1st given", new ExpressionRunner() {
 			public void expression(Arguments args) {}
 		})
 		.and("2nd given", new ExpressionRunner() {
@@ -32,10 +38,59 @@ public class GivenTest {
 		
 		assertThat(given.getAllGiven().size()).isEqualTo(2);
 	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void andInputIsNull() {
+		Scenario scenario = new Scenario(userStory, "And input is Null");
+		new Given(scenario, "1st given", new ExpressionRunner() {
+			public void expression(Arguments args) {}
+		})
+		.and(null, new ExpressionRunner() {
+			public void expression(Arguments args) {}
+		});
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void andInputIsEmpty() {
+		Scenario scenario = new Scenario(userStory, "And input is Empty");
+		new Given(scenario, "1st given", new ExpressionRunner() {
+			public void expression(Arguments args) {}
+		})
+		.and("", new ExpressionRunner() {
+			public void expression(Arguments args) {}
+		});
+	}
 
+	@Test(expected=IllegalArgumentException.class)
+	public void whenExpressionIsNull() {
+		Scenario scenario = new Scenario(userStory, "when() expression Is Null");
+		Given given = new Given(scenario, "1st given", new ExpressionRunner() {
+			public void expression(Arguments args) {}
+		});
+		
+		String when = null;
+		given.when(when, new ExpressionRunner() {
+			public void expression(Arguments args) {}
+		});
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void whenExpressionIsEmpty() {
+		Scenario scenario = new Scenario(userStory, "when() expression Is Emptyl");
+		Given given = new Given(scenario, "1st given", new ExpressionRunner() {
+			public void expression(Arguments args) {}
+		});
+		
+		String when = "";
+		given.when(when, new ExpressionRunner() {
+			public void expression(Arguments args) {}
+		});
+	}
+	
 	@Test
-	public void testWhen() {
-		Given given = new Given(new UserStory("story"), "1st given", new ExpressionRunner() {
+	public void whenNotThrowingException() {
+		Scenario scenario = new Scenario(userStory, "when() is not throwing exception");
+		Given given = new Given(scenario, "1st given", new ExpressionRunner() {
 			public void expression(Arguments args) {}
 		});
 		
