96a50eb891ef0261e1eac1532b69549f1d129730
mahlatsem <matt.mahlatse@gmail.com>
2016-04-07 15:14:22 +0200

    sending Report object to report runner

--- src/main/java/com/codecheck123/dumbbdd/When.java
+++ src/main/java/com/codecheck123/dumbbdd/When.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import com.codecheck123.dumbbdd.report.Report;
 import com.codecheck123.dumbbdd.report.ReportRunner;
 
 public class When extends AbstractBDD {
@@ -14,33 +15,50 @@ public class When extends AbstractBDD {
 	private static final ExecutorService es = Executors.newCachedThreadPool();
 
 	/**
-	 * Throws NullPointerException if parameters given or bddExpression is Null 
+	 * Throws IllegalArgumentException if parameters given or bddExpression is Null 
 	 * @param given
 	 * @param bddExpression
 	 */
 	When(Given given, String bddExpression){
 		if(given == null){
-			throw new NullPointerException("Given may not be NULL");
+			throw new IllegalArgumentException("Given may not be NULL");
 		}
-		if(bddExpression == null || bddExpression.isEmpty()){
-			throw new NullPointerException("BddExpression may not be NULL or Empty");
-		}
+		Util.checkForNotNullOrEmpty(bddExpression);
 		this.given = given;
 		this.when = bddExpression;
 	}
 
 	public void then(final String then, ExpressionRunner runner){
-		String story = given.getUserStory().getStory();
-		List<String> allGiven = given.getAllGiven();
+		Util.checkForNotNullOrEmpty(then);
+		
 		try{
 			evaluateExpression(then, runner);
 		}catch(AssertionError e){
 			assErr = e;
 			throw e;
 		}finally{
+			Report report = buildReport(then);
 			es.execute(
-				new ReportRunner(story, allGiven, when, then, assErr)
+				new ReportRunner(report)
 			);
 		}
+	}
+
+	private Report buildReport(String then) {
+		Scenario scenario = given.getScenario();
+		List<String> allGiven = given.getAllGiven();
+		UserStory userStory = scenario.getUserStory();
+		
+		Report report = new Report.ReportBuilder()
+				.setUserStoryTitle(userStory.getTitle())
+				.setUserStory(userStory.getStory())
+				.setScenarioTitle(scenario.getTitle())
+				.setAllGiven(allGiven)
+				.setWhen(when)
+				.setThen(then)
+				.setAssertionError(assErr)
+				.build();
+
+		return report;
 	}
 }
