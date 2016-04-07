63d86f25134cc0a7b1a0a77d23fb8e02422d758c
mahlatsem <matt.mahlatse@gmail.com>
2016-04-07 15:13:13 +0200

    renaming method names and using IllegalArgumentException

--- src/test/java/com/codecheck123/dumbbdd/UserStoryTest.java
+++ src/test/java/com/codecheck123/dumbbdd/UserStoryTest.java
@@ -1,30 +1,87 @@
 package com.codecheck123.dumbbdd;
 
 import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 
-import com.codecheck123.dumbbdd.Arguments;
-import com.codecheck123.dumbbdd.EmptyExpressionException;
-import com.codecheck123.dumbbdd.ExpressionRunner;
 import com.codecheck123.dumbbdd.UserStory;
 
 public class UserStoryTest {
 	
-	@Test(expected=EmptyExpressionException.class)
-	public void testNewUserStory_NULL_Story(){
-		new UserStory(null);
+	@Test(expected=IllegalArgumentException.class)
+	public void userStoryTitleIsNull(){
+		new UserStory.WithTitle(null);
 	}
 	
-	@Test(expected=EmptyExpressionException.class)
-	public void testNewUserStory_EmptyString(){
-		new UserStory("");
+	@Test(expected=IllegalArgumentException.class)
+	public void userStoryTitleIsEmpty(){
+		new UserStory.WithTitle("");
 	}
-
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void userStoryRoleIsEmpty(){
+		new UserStory.WithTitle("UserStory Role is Empty")
+		.asA("");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void userStoryRoleIsNull(){
+		new UserStory.WithTitle("UserStory Role is Null")
+		.asA(null);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void userStoryGoalOrDesireIsNull(){
+		new UserStory.WithTitle("UserStory GoalOrDesire is Null")
+		.asA("Developer")
+		.iWant(null);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void userStoryGoalOrDesireIsEmpty(){
+		new UserStory.WithTitle("UserStory GoalOrDesire is Empty")
+		.asA("Developer")
+		.iWant("");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void userStoryActionIsEmpty(){
+		new UserStory.WithTitle("UserStory Action is Empty")
+		.asA("Developer")
+		.iCan("");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void userStoryActionIsNull(){
+		new UserStory.WithTitle("UserStory Action is Null")
+		.asA("Developer")
+		.iCan(null);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void userStoryProvidedBenefitIsNull(){
+		new UserStory.WithTitle("UserStory ProvidedBenefit is Null")
+		.asA("Developer")
+		.iCan("Perfom some action")
+		.soThat(null);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void userStoryProvidedBenefitIsEmpty(){
+		new UserStory.WithTitle("UserStory ProvidedBenefit is Empty")
+		.asA("Developer")
+		.iCan("Perfom some action")
+		.soThat("");
+	}
+	
 	@Test
-	public void testCallingGivenNotNull(){
-		UserStory story = new UserStory("some text");
-		assertNotNull(story.given("some input", new ExpressionRunner() {
-			public void expression(Arguments args) {}
-		}));
+	public void userStoryIsCreatedWithoutException(){
+		UserStory userStory = new UserStory.WithTitle("UserStory is created without exception")
+				.asA("Developer")
+				.iWant("to write a UserStory")
+				.soThat("I can have it alongside my code")
+				.create();
+		
+		assertNotNull(userStory);
 	}
 }
