829cf178154b13ae7662d9d968c031d477ae23f4
mahlatsem <matt.mahlatse@gmail.com>
2016-04-07 15:15:37 +0200

    null check

--- src/main/java/com/codecheck123/dumbbdd/AbstractBDD.java
+++ src/main/java/com/codecheck123/dumbbdd/AbstractBDD.java
@@ -4,10 +4,17 @@ import java.util.List;
 
 public abstract class AbstractBDD {
 	
+	private static final String NULL_EXPRESSION_RUNNER_MESSAGE = "ExpressionRunner may not be Null"
+			+ " because it requires code to run as the action stated in the input expression\n"
+			+ "See Example: https://github.com/mahlatsem/dumb-bdd";
+
 	AbstractBDD(){}
 	
-	protected void evaluateExpression(String givenInput, ExpressionRunner runner) {
-		List<String> argsCollection = ArgumentsParser.parseExpressionToArguments(givenInput);
+	protected void evaluateExpression(String bddExpression, ExpressionRunner runner) {
+		if(runner == null){
+			throw new IllegalArgumentException(NULL_EXPRESSION_RUNNER_MESSAGE);
+		}
+		List<String> argsCollection = ArgumentsParser.parseExpressionToArguments(bddExpression);
 		Arguments args = new Arguments(argsCollection);
 		runner.expression(args);
 	}
